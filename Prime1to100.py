29d630528c9e52893411cce3e464b6350eb404e9
Lewis Ryan <66630538+lewismichaelbbc@users.noreply.github.com>
2020-06-08 22:25:37 +0100

    Update Prime1to100.py

--- Prime1to100.py
+++ Prime1to100.py
@@ -3,7 +3,7 @@ print("Displaying Prime Numbers Between 1 and 100\n")
 
 for PrimeN in range (1, 101):              # LOOP: Numbers 1-100 will be processed.
     CountN = 0
-    for i in range(2, (PrimeN//2 + 1)):    # IF the number is 'Between 2' and the 'PrimeN Divide by 2, plus 1'.
+    for i in range(2, (PrimeN//2 + 1)):    # LOOP: The number is 'Between 2' and the 'PrimeN Divide by 2, plus 1'.
         if(PrimeN % i == 0):               # Checks for a remainder / Decimal place. Proceed to next line, if 0
             CountN = CountN + 1            # Adds 1 to CountN to stop the next if statement from functioning.
             break                          # Breaks the loop to then move onto the next statement.
